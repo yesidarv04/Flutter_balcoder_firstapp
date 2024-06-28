2d1585671c7652fe9a18458530c12cffff1edac1
Yesidarv04 <yesidarv04@gmail.com>
2024-06-28 16:43:13 -0500

    Dart structure

---
+++ lib/utils/theme/app_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const Color primaryColor = Colors.black;
+  static const Color secondColor = Color(0xFFa1a1a1);
+  static const TextStyle titleStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+}
+
